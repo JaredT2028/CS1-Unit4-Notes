493b7c2a7c62e77ae5cfa58d3ff172b6cdaf2c75
JaredT2028 <jared.taitz2028@gbwl.org>
2026-04-06 12:52:48 +0000

    started js notes

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// SINGLE LINE COMMENT
+/* MULTI-LINE COMMENT */
+
+/* The JS code below is an example 
+STATEMENT (instruction/command) 
+every statement ends in a SEMICOLON */
 console.log("hello world");
+
+
+
