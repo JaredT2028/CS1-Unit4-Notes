5a3f6d9174c0a0ef0e8110caeec407e0358ca7a0
JaredT2028 <jared.taitz2028@gbwl.org>
2026-04-13 12:46:34 +0000

    learned about variables

--- script.js
+++ script.js
@@ -4,7 +4,28 @@
 /* The JS code below is an example 
 STATEMENT (instruction/command) 
 every statement ends in a SEMICOLON */
-console.log("hello world");
+console.log("welcome to JS");
 
+// VARIABLES are names storage containers for data 
 
+// DECLARE a new variable (make space)
+let username;
+
+// ASSIGN a value to. a varaible 
+username = "jaredtaitz14";
+
+// see the value stored in a variable 
+console.log(username);
+
+// DECLARE + ASSIGN in one line 
+let pokemon = "pikachu";
+let luckyNum = 9;
+
+// look at our new variables 
+console.log(pokemon);
+console.log(luckyNum);
+
+// Change the value stored in a var
+luckyNum = 15;
+console.log(luckyNum);
 
